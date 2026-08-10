9bc65dbcb4bf78a9d728842803cd9ffa99418d63
proav2323 <108835201+proav2323@users.noreply.github.com>
2026-08-10 17:09:28 +0530

    andorid_notification_should_work_now

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
     // END: FlutterFire Configuration
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Official Google Services dependency reference
+    id("com.google.gms.google-services") version "4.5.0" apply false
+    id("com.android.application")
+    // Apply the Google Services plugin
+    id("com.google.gms.google-services")
 }
 
 android {
